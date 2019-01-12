221b0facf7c213f04a5302e9ee68f9bd1e0b0045
Olfa Maslah <maslah.olfa@gmail.com>
2019-01-12 09:49:07 -0500

    project init

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	//create balls
+	const move = () =>{
+		var x = document.body.offsetHeight-element.clientHeight;
+		var y = document.body.offsetWidth-element.clientWidth;
+		var randomX = Math.floor(Math.random()*x);
+		var randomY = Math.floor(Math.random()*y);
+		return [randomX,randomY];
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+});
