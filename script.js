4e0129f2649d8ac8c802b3df4febd6050540d78b
Olfa Maslah <maslah.olfa@gmail.com>
2019-01-14 08:36:32 -0500

    version-avancé

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $('#start').on('click', function(event) {
         event.preventDefault();
         console.log('Play start!');
-        document.getElementById("intro").style.display = "none";
+        $('#intro').hide();
         counter();
         randChangeColor();
     });
@@ -31,7 +31,7 @@ $(document).ready(function() {
     });
 
     // 2) increase the score by 1
-    // 3) randomly select 1 figure, remove the class of bleu-circle, replace it with the class of red-circle
+    // 3) randomly select 1 figure, remove the class of bleu-circle, replace it with the class of yellow-circle
     //Step4      
     const clickBallon = (hasClass, ballon) => {
         if (hasClass) {
@@ -39,12 +39,11 @@ $(document).ready(function() {
             userScore = userScore + 1;
             console.log(userScore);
             //put the userScore inside of html to show the score
-            userScore = 0;
-            document.getElementById('scoreUser').innerHTML = "Score: " + userScore;
+            document.getElementById('scoreUser ').innerHTML = "Score = " + userScore;
             return randChangeColor();
         }
         userScore = userScore - 1;
-        document.getElementById('scoreUser').innerHTML = "Score = " + userScore;
+        document.getElementById('scoreUser ').innerHTML = "Score = " + userScore;
         console.log(userScore);
         return;
     };
@@ -53,53 +52,50 @@ $(document).ready(function() {
 
 
     //TIMER//	
-    parseInt(userScore);
     //// when the timer runs out
     // find all the figures with a class of  yellow-circle, remove the yellow-circle class and replace with a class of bleu-circle
     // show a "game is over" modal with the score of the user
 
     const decount = function(i) {
-        let temps = i + "s";
-        document.getElementById('count').innerHTML = temps;
+        document.getElementById('count').innerHTML = i + "s";
         if (i === 0) {
-
-            let gameOverMessage = "Game over, your score is " + userScore;
-            document.getElementById('end-game').innerHTML = gameOverMessage;
+            document.getElementById('end-game ').innerHTML = "Game over, your score is " + userScore;
             $('.yellow-circle').removeClass('yellow-circle');
-            document.getElementById("scoreUser").style.display = "none";
+            $('#scoreUser ').css("display", "none");
             // when the user clicks the play again button, repeat randomly select 1 figure, remove the class of bleu-circle, replace it with the class of yellow-circle AND start the timer .
         }
         if ($('#end').on('click', function() { //when user click on stop button, remove class of yellow-circle and hide the scoreUser and the timer
-                let gameOverMessage = "Game over, your score is " + userScore;
-                document.getElementById('end-game').innerHTML = gameOverMessage;
+
+                document.getElementById('end-game ').innerHTML = "Game over, your score is " + userScore;
+                $('end-game ').css("display", "none");
                 $('.yellow-circle').removeClass('yellow-circle');
-                document.getElementById("scoreUser").style.display = "none";
+                $('#userScore ').css("display", "none");
                 time = 0;
-                document.getElementById("count").style.display = "none";
+                $('#count').css("display", "none");
 
 
             }));
 
 
-        if ($('#start').on('click', function() { //to restart without reload the page BUT I had an issue, it is like the timer that I stop continue to decrease and when I press #start button, the both counting play et the same time, I gave up because it's midhight, after a lot of hours to try I can't be focus
-                time = 0;
-                userScore = 0;
-                document.getElementById("count").style.display = "block";
-                document.getElementById('end-game').innerHTML = "Stay focus";
-                document.getElementById("scoreUser").style.display = "block";
+        $('#start').on('click', function() { //to restart without reload the page BUT I had an issue, it is like the timer that I stop continue to decrease and when I press #start button, the both counting play et the same time, I gave up because it's midhight, after a lot of hours to try I can't be focus
+            time = 0;
+            userScore = 0;
+            $('#count').css("display", "block");
+            document.getElementById('end-game ').innerHTML = "Stay focus";
+            $('#userScore ').css("display", "block");
 
 
-            }));
+        });
 
 
     }
 
 
-    let time = 0;
+
     // for loop to make the timer
 
     const counter = () => {
-        time = 0;
+        let time = 0;
         for (var i = 20; i > -1; i--) { //to make i decrement from 20 to 0
             setTimeout((function(s) {
                 return function() {
