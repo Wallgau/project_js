ae152a05902fda51929e4ad78de1b2d6beda8d78
Olfa Maslah <maslah.olfa@gmail.com>
2019-01-14 01:19:20 -0500

    modif bof

--- script.js
+++ script.js
@@ -1,46 +1,31 @@
 //step1
 $(document).ready(function() {
-
-
     //event listener
-    $('.play').on('click', function(event) {
+    $('#start').on('click', function(event) {
         event.preventDefault();
         console.log('Play start!');
         document.getElementById("intro").style.display = "none";
         counter();
         randChangeColor();
-
-
-
     });
-    draw();
-    let ballon = $('.ballons')
 
-    function getRandomPosition(ballon) {
-        var x = document.body.offsetHeight - ballon.clientHeight;
-        var y = document.body.offsetWidth - ballon.clientWidth;
-        var randomX = Math.floor(Math.random() * x);
-        var randomY = Math.floor(Math.random() * y);
-        return [randomX, randomY];
-    }
-
-
-    // When the user clicks the start button, randomly select 1 figure, add the class of red-circle ( AND start the timer )
+    // When the user clicks the start button, randomly select 1 figure, add the class of yellow-circle ( AND start the timer )
     //step2
+
     const randChangeColor = () => {
-        const numberOfBallon = $('main figure').length;
+        const numberOfBallon = $('main figure').length; // to get a number and be able to use Math.random to pick a ball randomly
         const randNumber = Math.floor(Math.random() * numberOfBallon);
         const randBallon = $(`main figure:nth-child(${randNumber})`);
         randBallon.addClass('yellow-circle');
     };
 
     // when the user clicks on a figure
-    // - IF the figure has a class of red-circle, 
+    // - IF the figure has a class of yellow-circle, 
     // 1) remove the class of red-circle, replace it with with the class of blue-circle
     //Step 3
     $('figure').on('click', function(event) {
         event.preventDefault();
-        const ballon = $(this); //ici this fait rÃ©fÃ©rence au ballon qu'on vient de cliquer, c'est ce ballon pas tous les ballon
+        const ballon = $(this); //ici this fait référence au ballon qu'on vient de cliquer, c'est ce ballon pas tous les ballon
         const figureHasClass = ballon.hasClass('yellow-circle'); //ici implicitement Ã§a return true
         clickBallon(figureHasClass, ballon);
     });
@@ -53,16 +38,15 @@ $(document).ready(function() {
             ballon.removeClass('yellow-circle');
             userScore = userScore + 1;
             console.log(userScore);
-            //put the userScore inside of html to show the score 
-            document.getElementById('scoreUser').innerHTML = "Score = " + userScore;
+            //put the userScore inside of html to show the score
+            userScore = 0;
+            document.getElementById('scoreUser').innerHTML = "Score: " + userScore;
             return randChangeColor();
         }
         userScore = userScore - 1;
         document.getElementById('scoreUser').innerHTML = "Score = " + userScore;
         console.log(userScore);
         return;
-
-
     };
     let userScore = 0;
     //when the counter is at 0, message to get out or continue the game
@@ -71,8 +55,9 @@ $(document).ready(function() {
     //TIMER//	
     parseInt(userScore);
     //// when the timer runs out
-    // find all the figures with a class of red-circle, remove the red-circle class and replace with a class of bleu-circle
-    // show a "game is over" modal with the score of the user, and the ability to "Play Again"
+    // find all the figures with a class of  yellow-circle, remove the yellow-circle class and replace with a class of bleu-circle
+    // show a "game is over" modal with the score of the user
+
     const decount = function(i) {
         let temps = i + "s";
         document.getElementById('count').innerHTML = temps;
@@ -80,31 +65,42 @@ $(document).ready(function() {
 
             let gameOverMessage = "Game over, your score is " + userScore;
             document.getElementById('end-game').innerHTML = gameOverMessage;
-            $('.red-circle').removeClass('yellow-circle');
+            $('.yellow-circle').removeClass('yellow-circle');
             document.getElementById("scoreUser").style.display = "none";
-            document.getElementById("play-again").style.display = "block"; // when the user clicks the play again button, repeat randomly select 1 figure, remove the class of bleu-circle, replace it with the class of red-circle AND start the timer AND close the modal : because this 2 buttons have the same class so I used their class to start the game so they do the same thing.
-            document.getElementById("start").style.display = "none";
+            // when the user clicks the play again button, repeat randomly select 1 figure, remove the class of bleu-circle, replace it with the class of yellow-circle AND start the timer .
         }
-        $('#end').on('click', function() {
-            if (i > 0) {
+        if ($('#end').on('click', function() { //when user click on stop button, remove class of yellow-circle and hide the scoreUser and the timer
                 let gameOverMessage = "Game over, your score is " + userScore;
                 document.getElementById('end-game').innerHTML = gameOverMessage;
-                $('.red-circle').removeClass('yellow-circle');
+                $('.yellow-circle').removeClass('yellow-circle');
                 document.getElementById("scoreUser").style.display = "none";
+                time = 0;
                 document.getElementById("count").style.display = "none";
 
 
+            }));
 
 
+        if ($('#start').on('click', function() { //to restart without reload the page BUT I had an issue, it is like the timer that I stop continue to decrease and when I press #start button, the both counting play et the same time, I gave up because it's midhight, after a lot of hours to try I can't be focus
+                time = 0;
+                userScore = 0;
+                document.getElementById("count").style.display = "block";
+                document.getElementById('end-game').innerHTML = "Stay focus";
+                document.getElementById("scoreUser").style.display = "block";
 
-            }
-        })
+
+            }));
+
+
     }
 
 
+    let time = 0;
+    // for loop to make the timer
+
     const counter = () => {
-        let time = 0;
-        for (var i = 20; i > -1; i--) {
+        time = 0;
+        for (var i = 20; i > -1; i--) { //to make i decrement from 20 to 0
             setTimeout((function(s) {
                 return function() {
                     decount(s);
@@ -114,15 +110,4 @@ $(document).ready(function() {
             time += 1000;
         }
     }
-
-
-
-    //message of end of timer
-
-
-
-
-
-
-
 });
