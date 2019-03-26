e58f1ba8946ee53fa7a76bd5cd9e7fb7148117fd
Olfa <maslah.olfa@gmail.com>
2019-03-26 16:02:28 -0400

    change grid to flex

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //step1
-$(document).ready(function() {
+$(document).ready(function () {
     //event listener
-    $('#start').on('click', function(event) {
+    $('#start').on('click', function (event) {
         event.preventDefault();
         console.log('Play start!');
         $('#intro').hide();
         $('#count').css("display", "block");
-        document.getElementById('end-game ').innerHTML = "Stay focus";
+        document.getElementById('end-game').innerHTML = "Stay focus";
         userScore = 0;
         $('#userScore ').css("display", "block");
         counter();
@@ -27,7 +27,7 @@ $(document).ready(function() {
     // - IF the figure has a class of yellow-circle, 
     // 1) remove the class of red-circle, replace it with with the class of blue-circle
     //Step 3
-    $('figure').on('click', function(event) {
+    $('figure').on('click', function (event) {
         event.preventDefault();
         const ballon = $(this); //ici this fait référence au ballon qu'on vient de cliquer, c'est ce ballon pas tous les ballon
         const figureHasClass = ballon.hasClass('yellow-circle'); //ici implicitement Ã§a return true
@@ -43,11 +43,11 @@ $(document).ready(function() {
             userScore = userScore + 1;
             console.log(userScore);
             //put the userScore inside of html to show the score
-            document.getElementById('scoreUser ').innerHTML = "Score = " + userScore;
+            document.getElementById('scoreUser').innerHTML = "Score = " + userScore;
             return randChangeColor();
         }
         userScore = userScore - 1;
-        document.getElementById('scoreUser ').innerHTML = "Score = " + userScore;
+        document.getElementById('scoreUser').innerHTML = "Score = " + userScore;
         console.log(userScore);
         return;
     };
@@ -60,24 +60,24 @@ $(document).ready(function() {
     // find all the figures with a class of  yellow-circle, remove the yellow-circle class and replace with a class of bleu-circle
     // show a "game is over" modal with the score of the user
 
-    const decount = function(i) {
+    const decount = function (i) {
         document.getElementById('count').innerHTML = i + "s";
         if (i === 0) {
-            document.getElementById('end-game ').innerHTML = "Game over, your score is " + userScore;
+            document.getElementById('end-game').innerHTML = "Game over, your score is " + userScore;
             $('.yellow-circle').removeClass('yellow-circle');
             $('#scoreUser ').css("display", "none");
             // when the user clicks the play again button, repeat randomly select 1 figure, remove the class of bleu-circle, replace it with the class of yellow-circle AND start the timer .
         }
-        if ($('#end').on('click', function() { //when user click on stop button, remove class of yellow-circle and hide the scoreUser and the timer
+        if ($('#end').on('click', function () { //when user click on stop button, remove class of yellow-circle and hide the scoreUser and the timer
 
-                document.getElementById('end-game ').innerHTML = "Game over, your score is " + userScore;
-                $('end-game ').css("display", "none");
-                $('.yellow-circle').removeClass('yellow-circle');
-                $('#userScore ').css("display", "none");
-                time = 0;
-                $('#count').css("display", "none");
-                setTimeout(function() { location.reload() }, 1000);
-            }));
+            document.getElementById('end-game').innerHTML = "Game over, your score is " + userScore;
+            $('#end-game').css("display", "none");
+            $('.yellow-circle').removeClass('yellow-circle');
+            $('#userScore ').css("display", "none");
+            time = 0;
+            $('#count').css("display", "none");
+            setTimeout(function () { location.reload() }, 1000);
+        }));
 
 
 
@@ -89,8 +89,8 @@ $(document).ready(function() {
         let time = 0;
 
         for (var i = 20; i > -1; i--) { //to make i decrement from 20 to 0
-            setTimeout((function(s) {
-                return function() {
+            setTimeout((function (s) {
+                return function () {
                     decount(s);
                 }
             })(i), time);
